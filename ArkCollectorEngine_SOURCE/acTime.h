cd54f754684b46835d27a13e9e47a9c06c70af8b
rnrn213 <usan213@gmail.com>
2024-11-26 15:42:11 +0900

    feat: Add Time class

---
+++ ArkCollectorEngine_SOURCE/acTime.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "CommonInclude.h"
+
+namespace ac
+{
+	class Time
+	{
+	public:
+		void Initialize();
+		void Tick();
+		void Render(HDC hdc);
+
+	private:
+		LARGE_INTEGER mCpuFrequency;
+		LARGE_INTEGER mPrevFrequency;
+		LARGE_INTEGER mCurrentFrequency;
+
+		float mDeltaTime;
+	};
+}
+
