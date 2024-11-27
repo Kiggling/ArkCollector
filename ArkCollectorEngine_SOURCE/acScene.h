ece31d263f2f1676d551c52ff1d7fc89e032fc94
rnrn213 <usan213@gmail.com>
2024-11-27 18:10:42 +0900

    feat: Scene 클래스 추가

---
+++ ArkCollectorEngine_SOURCE/acScene.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "CommonInclude.h"
+#include "acEntity.h"
+#include "acLayer.h"
+
+namespace ac
+{
+	class Scene : public Entity
+	{
+	public:
+		Scene();
+		~Scene();
+
+		virtual void Initialize();
+		virtual void Update();
+		virtual void LateUpdate();
+		virtual void Render(HDC hdc);
+
+		virtual void OnEnter();
+		virtual void OnExit();
+
+		__forceinline Layer* GetLayer(const enums::eLayerType type) { return mLayers[(UINT)type]; }
+
+	private:
+		void createLayers();
+
+	private:
+		std::vector<Layer*> mLayers;
+	};
+}
