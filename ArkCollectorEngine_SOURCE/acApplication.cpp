592677035889caf23ba7ddbf508f55baf6688967
rnrn213 <usan213@gmail.com>
2024-11-27 17:07:21 +0900

    feat: Double Buffering 구현

--- ArkCollectorEngine_SOURCE/acApplication.cpp
+++ ArkCollectorEngine_SOURCE/acApplication.cpp
@@ -1,14 +1,20 @@
 #include "acApplication.h"
-
+#include "acTime.h"
+#include "acInput.h"
 
 namespace ac
 {
 	Application::Application()
 		: mHwnd(nullptr)
 		, mHdc(nullptr)
+		, mBackHdc(nullptr)
+		, mBackBitmap(nullptr)
+		, mWidth(0)
+		, mHeight(0)
 	{
 
 	}
+
 	Application::~Application()
 	{
 
@@ -16,24 +22,80 @@ namespace ac
 
 	void Application::Initialize(HWND InHwnd, UINT width, UINT height)
 	{
-		
+		adjustWindowRect(InHwnd, width, height);
+		createBuffer(width, height);
+
+		Input::Initialize();
+		Time::Initialize();
 	}
+
 	void Application::Run()
 	{
 		Update();
 		LateUpdate();
 		Render();
 	}
+
 	void Application::Update()
 	{
-	
+		Input::Update();
+		Time::Update();
 	}
+
 	void Application::LateUpdate()
 	{
 		
 	}
+
 	void Application::Render()
 	{
+		// mBackHdc 지우기
+		clearRenderTarget();
+
+		// 화면에 출력하고자 하는 클래스의 Render 함수 호출 (인자로 mBackHdc 사용)
 		
+
+		// mBackHdc에 그린 것들을 mHdc로 복사
+		copyRenderTarget(mBackHdc, mHdc);
+	}
+
+	void Application::adjustWindowRect(HWND hwnd, UINT width, UINT height)
+	{
+		mHwnd = hwnd;
+		mHdc = GetDC(hwnd);
+
+		RECT rect = { 0, 0, width, height };
+		AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+
+		mWidth = rect.right - rect.left;
+		mHeight = rect.bottom - rect.top;
+
+		SetWindowPos(hwnd, nullptr, 0, 0, mWidth, mHeight, 0);
+		ShowWindow(hwnd, true);
+	}
+
+	void Application::createBuffer(UINT width, UINT height)
+	{
+		mBackBitmap = CreateCompatibleBitmap(mHdc, width, height);
+		mBackHdc = CreateCompatibleDC(mHdc);
+
+		HBITMAP oldBitmap = (HBITMAP)SelectObject(mBackHdc, mBackBitmap);
+		DeleteObject(oldBitmap);
+	}
+
+	void Application::clearRenderTarget()
+	{
+		HBRUSH grayBrush = (HBRUSH)CreateSolidBrush(RGB(128, 128, 128));
+		HBRUSH oldBrush = (HBRUSH)SelectObject(mBackHdc, grayBrush);
+
+		Rectangle(mBackHdc, -1, -1, mWidth + 1, mHeight + 1);
+
+		(HBRUSH)SelectObject(mBackHdc, oldBrush);
+		DeleteObject(grayBrush);
+	}
+
+	void Application::copyRenderTarget(HDC source, HDC dest)
+	{
+		BitBlt(dest, 0, 0, mWidth, mHeight, source, 0, 0, SRCCOPY);
 	}
 }
