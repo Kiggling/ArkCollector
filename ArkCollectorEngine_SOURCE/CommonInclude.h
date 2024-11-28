eb9a0f2fc46cea7168f07bf9d64cf46860b59f64
rnrn213 <usan213@gmail.com>
2024-11-29 00:20:50 +0900

    feat: 헤더 추가

--- ArkCollectorEngine_SOURCE/CommonInclude.h
+++ ArkCollectorEngine_SOURCE/CommonInclude.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <Windows.h>
+#include <assert.h>
 
 #include <vector>
 #include <algorithm>
 #include <string>
 #include <map>
+#include <functional>
+#include <filesystem>
 
 #include "acMath.h"
 #include "acEnums.h"
+
+#include <mmsystem.h>
+#include <dinput.h>
+#pragma comment(lib, "Msimg32.lib");
+#pragma comment(lib, "winmm.lib");
+
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib");
