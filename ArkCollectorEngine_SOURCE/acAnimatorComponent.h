4430affdc209845bb76115d2bd20e30c572bb1f7
rnrn213 <usan213@gmail.com>
2024-11-29 00:18:46 +0900

    feat: Animation, Animator Component 클래스 구현

---
+++ ArkCollectorEngine_SOURCE/acAnimatorComponent.h
@@ -0,0 +1,70 @@
+#pragma once
+#include "acComponent.h"
+#include "acAnimation.h"
+
+namespace ac
+{
+	class AnimatorComponent : public Component
+	{
+	public:
+		struct Event
+		{
+			void operator=(std::function<void()> func)
+			{
+				mEvent = std::move(func);
+			}
+
+			void operator()()
+			{
+				if (mEvent)
+					mEvent();
+			}
+
+			std::function<void()> mEvent;
+		};
+
+		struct Events
+		{
+			Event startEvent;
+			Event completeEvent;
+			Event endEvent;
+		};
+
+		AnimatorComponent();
+		~AnimatorComponent();
+
+		void Initialize() override;
+		void Update() override;
+		void LateUpdate() override;
+		void Render(HDC hdc) override;
+
+		void CreateAnimation(std::wstring& name
+			, graphics::Texture* spriteSheet
+			, math::Vector2 leftTop
+			, math::Vector2 size
+			, math::Vector2 offset
+			, UINT spriteLegth
+			, float duration);
+		void CreateAnimationByFolder(std::wstring& name
+			, const std::wstring& path
+			, math::Vector2 offset
+			, float duration);
+
+		Animation* FindAnimation(const std::wstring& name);
+		void PlayAnimation(const std::wstring& name, bool loop = true);
+
+		Events* FindEvents(const std::wstring& name);
+		std::function<void()>& GetStartEvent(const std::wstring& name);
+		std::function<void()>& GetCompleteEvent(const std::wstring& name);
+		std::function<void()>& GetEndEvent(const std::wstring& name);
+
+		bool IsComplete() { return mActiveAnimation->IsComplete(); }
+
+	private:
+		std::map<std::wstring, Animation*> mAnimations;
+		Animation* mActiveAnimation;
+		bool mbLoop;
+
+		std::map<std::wstring, Events*> mEvents;
+	};
+}
